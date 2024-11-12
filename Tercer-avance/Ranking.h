57bd7c5efb5417d1d1653e2de72b9e0a9e1bb19f
Sangg22 <142357065+Sangg22@users.noreply.github.com>
2024-11-12 14:00:05 -0600

    Update Ranking.h

--- Tercer-avance/Ranking.h
+++ Tercer-avance/Ranking.h
@@ -10,39 +10,73 @@
 class Ranking{
     private:
         std::vector<Player> players;
-        void swap(std::vector<Player>& v, int i, int j) {
-            Player aux = v[i];
-            v[i] = v[j];
-            v[j] = aux;
-        }
-    
-    public:
 
+        void merge(std::vector<Player>& v, int left, int mid, int right) {
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+
+            std::vector<Player> leftVec(n1);
+            std::vector<Player> rightVec(n2);
+
+            for (int i = 0; i < n1; i++) {
+                leftVec[i] = v[left + i];
+            }
+            for (int j = 0; j < n2; j++) {
+                rightVec[j] = v[mid + 1 + j];
+            }
+
+            int i = 0, j = 0, k = left;
+
+            while (i < n1 && j < n2) {
+                if (leftVec[i].getPoints() >= rightVec[j].getPoints()) {
+                    v[k] = leftVec[i];
+                    i++;
+                } else {
+                    v[k] = rightVec[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1) {
+                v[k] = leftVec[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2) {
+                v[k] = rightVec[j];
+                j++;
+                k++;
+            }
+        }
+
+        void mergeSort(std::vector<Player>& v, int left, int right) {
+            if (left < right) {
+                int mid = left + (right - left) / 2;
+                mergeSort(v, left, mid);
+                mergeSort(v, mid + 1, right);
+                merge(v, left, mid, right);
+            }
+        }
+
+    public:
         /*Análisis de complejidad:
         Mejor de los casos: O(1)
         Caso Promedio: O(1)
         Peor de los casos: O(1)
         */
-        void addPlayer(Player& player){
+        void addPlayer(Player& player) {
             players.push_back(player);
         }
 
         /*Análisis de complejidad:
-        Mejor de los casos: O(n)
-        Caso Promedio: O(n^2)
-        Peor de los casos: O(n^2)
+        Mejor de los casos: O(n log n)
+        Caso Promedio: O(n log n)
+        Peor de los casos: O(n log n)
         */
-        void sortByPoints(){
-            for (int i = 1; i < players.size(); i++){
-                for(int j = i; j > 0; j--){
-                    if (players[j].getPoints() > players[j-1].getPoints()){
-                        swap(players, j, j - 1);
-                    }
-                    else{
-                        break;
-                    }
-                }
-            }
+        void sortByPoints() {
+            mergeSort(players, 0, players.size() - 1);
         }
 
         /*Análisis de complejidad:
@@ -57,7 +91,7 @@ class Ranking{
                     player.showInfo();
                     return;
                 }
-             }
+            }
             std::cout << "Player not found.\n";
         }
 
@@ -68,13 +102,11 @@ class Ranking{
         */
         void showRanking() {
             std::cout << "Ranking of players by points:\n";
-            for(auto& player : players) {
+            for (auto& player : players) {
                 player.showInfo();
                 std::cout << "-------------------\n";
             }
         }
 };
-
-
 
 #endif
