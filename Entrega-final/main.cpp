09c6fff8348af4c704c8fb23661ed1419859c8a0
Sangg22 <142357065+Sangg22@users.noreply.github.com>
2024-11-26 14:24:14 -0600

    Update main.cpp

--- Entrega-final/main.cpp
+++ Entrega-final/main.cpp
@@ -1,5 +1,148 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <ctime>
+#include <iomanip>
+#include "Player.h"
+#include "Ranking.h"
+#include "Team.h"
+#include "Tournament.h"
+
+void loadPlayers(const std::string& Basketballplayers2, Ranking& ranking, std::vector<Team>& teams) {
+    std::ifstream csv(Basketballplayers2);
+    std::string line, name, position, number, points, height, teamName;
+
+    if (!csv.is_open()) {
+        std::cerr << "Error: Could not open " << Basketballplayers2 << std::endl;
+        return;
+    }
+
+    std::getline(csv, line);  
+    while (std::getline(csv, line)) {
+        std::stringstream ss(line);
+
+        std::getline(ss, name, ',');
+        std::getline(ss, position, ',');
+        std::getline(ss, number, ',');
+        std::getline(ss, points, ',');
+        std::getline(ss, height, ',');
+        std::getline(ss, teamName, ',');
+
+        int backnumber = std::stoi(number);
+        int pointsscored = std::stoi(points);
+        float heightplayer = std::stof(height);
+
+        Player player(name, position, backnumber, pointsscored, heightplayer, teamName);
+        ranking.addPlayer(player);
+
+        bool teamFound = false;
+        for (auto& team : teams) {
+            if(team.getName() == teamName){
+                team.addPlayer(player);
+                teamFound = true;
+                break;
+            }
+        }
+        if(!teamFound){
+            Team newTeam(teamName);
+            newTeam.addPlayer(player);
+            teams.push_back(newTeam);
+        }
+    }
+    csv.close();
+}
+
+// Mostrar menú
+void showMenu() {
+    std::cout << "=====================\n";
+    std::cout << "   Menu of Ranking\n";
+    std::cout << "=====================\n";
+    std::cout << "1. Show Players\n";
+    std::cout << "2. Sort players by points\n";
+    std::cout << "3. Search player by name\n";
+    std::cout << "4. Show Teams\n";
+    std::cout << "5. Start tournament\n";
+    std::cout << "6. Exit\n";
+    std::cout << "Choose an option: ";
+}
+
+std::string generateUniqueFileName() {
+    std::ostringstream fileName;
+    std::time_t t = std::time(nullptr);
+    fileName << "tournament_results_" << std::put_time(std::localtime(&t), "%Y%m%d_%H%M%S") << ".csv";
+    return fileName.str();
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+    Ranking ranking;
+    std::vector<Team> teams;
+
+    std::string filename = "Basketballplayers2.csv";
+    loadPlayers(filename, ranking, teams);
+
+    Tournament tournament;
+    for (const auto& team : teams){
+        tournament.addTeam(team);
+    }
+
+    int opcion;
+
+    do {
+        showMenu();
+        std::cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                std::cout << "\nList of players:\n";
+                ranking.showRanking();
+                break;
+            case 2:
+                std::cout << "\nSorting players by points...\n";
+                ranking.sortByPoints();
+                std::cout << "Players arranged correctly.\n";
+                break;
+            case 3: {
+                std::string name;
+                std::cout << "Enter the name of the player to search: ";
+                std::cin.ignore(); 
+                std::getline(std::cin, name);
+                ranking.searchPlayerByName(name);
+                break;
+            }
+            case 4:
+                std::cout << "\nList of Teams:\n";
+                for (auto& team : teams){
+                    std::cout << "Team: " << team.getName() << "\n";
+                    team.showPlayersInfo();
+                    std::cout << "---------------------\n";
+                }
+                break;
+            case 5:
+                std::cout << "\nStarting the tournament...\n";
+                tournament.startTournament();
+
+                int csvOption;
+                std::cout << "Choose an option to save results:\n";
+                std::cout << "1. Create a new CSV file\n";
+                std::cin >> csvOption;
+
+                if (csvOption == 1) {
+                    std::string newFileName = generateUniqueFileName();
+                    tournament.updateCSV(newFileName);
+                    std::cout << "Results saved to new file: " << newFileName << std::endl;
+                }else {
+                    std::cout << "Invalid option.\n";
+                }
+            case 6:
+                std::cout << "Leaving the program...\n";
+                break;
+            default:
+                std::cout << "Invalid option. Try again.\n";
+                break;
+        }
+
+        std::cout << "\n";
+    } while (opcion != 6);
+
+    return 0;
 }
